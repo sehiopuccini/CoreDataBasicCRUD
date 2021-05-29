0b4b94b5d2626ae4d02244bef62d32537662ac28
windwail <windwail>
2021-05-29 19:53:33 +0300

    Initial Commit

---
+++ CoreDataBasicCRUD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataBasicCRUD
+//
+//  Created by Sergio on 29.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreDataBasicCRUD/CoreDataBasicCRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBasicCRUDApp.swift
+//  CoreDataBasicCRUD
+//
+//  Created by Sergio on 29.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBasicCRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
